286f4f009a26fefecd475770147b40e20ab6d13e
tvconss <tranvanconkg@gmail.com>
2026-02-27 12:09:34 +0700

    chore (google-oauth2): load config, add to root modeule

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import appConfig, { getEnvPath } from "./app.config";
 import { HealthModule } from "../health/health.module";
+import { GoogleOauth2Module } from "../google-oauth2/google-oauth2.module";
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { HealthModule } from "../health/health.module";
       load: [appConfig],
     }),
     HealthModule,
+    GoogleOauth2Module,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/google-oauth2/google-oauth2.module.ts
+++ src/modules/google-oauth2/google-oauth2.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { GoogleOauth2Service } from "./google-oauth2.service";
 import { GoogleOauth2Controller } from "./google-oauth2.controller";
+import { ConfigModule } from "@nestjs/config";
+import googleOauth2Config from "./google-oauth2.config";
 
 @Module({
+  imports: [ConfigModule.forFeature(googleOauth2Config)],
   controllers: [GoogleOauth2Controller],
   providers: [GoogleOauth2Service],
 })
