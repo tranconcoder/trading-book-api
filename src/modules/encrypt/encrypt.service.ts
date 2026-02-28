2d95d9c0165a23cad11ef5afe048fa442244d96c
tvconss <tranvanconkg@gmail.com>
2026-02-28 10:02:53 +0700

    feat (encrypt/generate-key-pair): create method to generate key pair RSA

--- src/modules/encrypt/encrypt.service.ts
+++ src/modules/encrypt/encrypt.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from "@nestjs/common";
+import { generateKeyPair } from "node:crypto";
+import { promisify } from "node:util";
+import { InternalServerError } from "@/core/response/errors";
+
+const generateKeyPairAsync = promisify(generateKeyPair);
 
 @Injectable()
-export class EncryptService {}
+export class EncryptService {
+  /**
+   * Generate a new RSA Key Pair (Private & Public)
+   * Default bit length: 2048
+   */
+  async generateRSAKeyPair(): Promise<{
+    publicKey: string;
+    privateKey: string;
+  }> {
+    try {
+      const { publicKey, privateKey } = await generateKeyPairAsync("rsa", {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+          type: "spki",
+          format: "pem",
+        },
+        privateKeyEncoding: {
+          type: "pkcs8",
+          format: "pem",
+        },
+      });
+
+      return { publicKey, privateKey };
+    } catch (error) {
+      throw new InternalServerError(
+        `Failed to generate RSA key pair: ${error instanceof Error ? error.message : "Unknown error"}`,
+      );
+    }
+  }
+}
